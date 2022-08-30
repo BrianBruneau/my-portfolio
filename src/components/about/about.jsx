bb533e0ea56b60d873543e069152dc1d18ba3aab
Brian Bruneau <brian8ax@gmail.com>
2022-08-30 11:39:30 -0500

    I just realized I have not committed in quite a while! How rude of me! I promise to commit way more often from here on out! Anyways, We added all our components, JSX, and styled with CSS, and some SASS variables. We build out each component of the site individually. It was a very interesting process. I ame excited to do some more of my own custom tweaking and personalization.

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
+
+
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>2+ Years Working Experience</small>
+            </article> 
+
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>5+ clients</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>10+ Projects completed</small>
+            </article>
+
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque blanditiis consequatur cumque doloribus eveniet totam necessitatibus aut. Fuga voluptates eos sint ipsa deserunt debitis vel recusandae soluta, non odit exercitationem.
+          </p>
+
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
