cb2b5da073283580fa380675d95e5d5830781866
Brian Bruneau <brian8ax@gmail.com>
2022-08-30 12:00:40 -0500

    fixing warnings in build in an attempt to get Vercel to succesfully build

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -7,7 +7,7 @@ import {IoLogoTwitter} from 'react-icons/io'
 const Footer = () => {
   return (
       <footer>
-        <a href="#" className='footer__logo'>BrianBruneau</a>
+        <a href="#home" className='footer__logo'>BrianBruneau</a>
 
         <ul className="permalinks">
           <li><a href="#home">Home</a></li>
